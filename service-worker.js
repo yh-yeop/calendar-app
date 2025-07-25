131ba52d45d934594cb67529f39a66c5d026eb78
yh-yeop <hoyajjang0115@naver.com>
2025-07-26 00:08:23 +0900

    init

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+const cacheName = 'calendar-app-v1';
+const filesToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(filesToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request))
+  );
+});
